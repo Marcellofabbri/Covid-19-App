55b72d1cfe8c4da26e38f6e0fe71ab3cd76f45a3
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-03-23 17:34:17 +0000

    call Coronavirus API and get data

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() => runApp(MaterialApp(
   home: Home(),
 ));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  void getData() async {
+
+    Response response = await get('https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=Italy', headers: {"x-rapidapi-key": "558013d577mshda14e3082866bccp17df82jsncdc9b261bdcc"});
+    Map data = jsonDecode(response.body);
+    print(data);
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override //redefines the build method otherwise inherited from StatelessWidget
   Widget build(BuildContext context) {
     return Scaffold(
